d3d024f24f5b457963339649e5ae8b8448cbf8ed
Azeem Rafique <urbancrime19@gmail.com>
2024-10-10 11:12:37 +0500

    code update

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import {StyleSheet} from 'react-native';
 import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -29,4 +28,3 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
